278a223462a291a2aa1db813ce66296aec874aa4
YeaItsJesus <122085651+YeaItsJesus@users.noreply.github.com>
2023-04-09 16:16:10 -0500

    files

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const noteData = require('./db/db.json');
+const PORT =  process.env.PORT || 3001;
+
+const app = express();
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
+
+app.get('/api/notes', (req, res) => res.json(noteData));
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
