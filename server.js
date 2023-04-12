0afc5c7b497843fc849e5918a29444c6defb81c5
YeaItsJesus <122085651+YeaItsJesus@users.noreply.github.com>
2023-04-12 18:15:35 -0500

    ability to add notes implemented

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const path = require('path');
-const noteData = require('./db/db.json');
-const PORT =  process.env.PORT || 3001;
+const express = require("express");
+const path = require("path");
+const noteData = require("./db/db.json");
+const PORT = process.env.PORT || 3001;
+
+console.log(noteData);
 
 const app = express();
 
@@ -9,16 +11,23 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-  });
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
-app.get('/api/notes', (req, res) => res.json(noteData));
+app.get("/api/notes", (req, res) => res.json(noteData));
+
+app.post("/api/notes", (req, res) => {
+  noteData.push(req.body);
+  //Add ID to NEW notes (body)
+  //fs.writefile
+  res.json(noteData);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
